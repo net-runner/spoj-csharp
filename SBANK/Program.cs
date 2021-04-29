6733731732b88874e6ccc925d508f425f44d9944
net-runner <slazynski.j@gmail.com>
2021-04-29 21:58:30 +0200

    Accepted 4.51

--- SBANK/Program.cs
+++ SBANK/Program.cs
@@ -1,37 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Numerics;
 
 namespace SBANK
 {
     class Program
     {
+        public class Amogus
+        {
+            public int ammount = 1;
+            public string key = "";
+
+            public Amogus(string k)
+            {
+                this.key = k;
+            }
+        }
         static string s = "";
         static int howManyAccounts = 0;
-        static Dictionary<string, int> aa;
+        static Dictionary<BigInteger, Amogus> aa;
+        static SortedDictionary<BigInteger, Amogus> bb;
+        public static BigInteger ToBigInteger(string value)
+        {
+            BigInteger result = 0;
+            for (int i = 0; i < value.Length; i++)
+            {
+                result = result * 10 + (value[i] - '0');
+            }
+            return result;
+        }
         static void Main(string[] args)
         {
             int tests = int.Parse(Console.ReadLine());
             int counter = 0;
-            SortedDictionary<string, int>[] w = new SortedDictionary<string, int>[tests];
             while (counter < tests)
             {
                 howManyAccounts = int.Parse(Console.ReadLine());
-                aa = new Dictionary<string, int>();
+                aa = new Dictionary<BigInteger, Amogus>();
                 for (var i = 0; i < howManyAccounts; i++)
                 {
                     s = Console.ReadLine();
-                    if (!aa.TryAdd(s, 1)) aa[s]++;
+                    BigInteger ss = ToBigInteger(s);
+                    if (!aa.TryAdd(ss, new Amogus(s))) aa[ss].ammount++;
                 }
-                w[counter] = new SortedDictionary<string, int>(aa);
+                bb = new SortedDictionary<BigInteger, Amogus>(aa);
+                foreach (var account in bb) Console.WriteLine(account.Value.key + account.Value.ammount);
                 if (counter + 1 != tests)
+                {
                     Console.ReadLine();
-                counter++;
-            }
-            for (var i = 0; i < tests; i++)
-            {
-                foreach (var account in w[i]) Console.WriteLine(account.Key + account.Value);
-                if (i + 1 != tests)
                     Console.WriteLine();
+                }
+                counter++;
             }
         }
     }
