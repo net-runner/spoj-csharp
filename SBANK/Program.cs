1179d3f967f14ceafa587dbe236370480b45dafd
net-runner <slazynski.j@gmail.com>
2021-05-01 10:56:07 +0200

    SBANK accepted - 1.79

--- SBANK/Program.cs
+++ SBANK/Program.cs
@@ -1,34 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Numerics;
 
 namespace SBANK
 {
     class Program
     {
-        public class Amogus
-        {
-            public int ammount = 1;
-            public string key = "";
-
-            public Amogus(string k)
-            {
-                this.key = k;
-            }
-        }
         static string s = "";
+        static string[] x;
         static int howManyAccounts = 0;
-        static Dictionary<BigInteger, Amogus> aa;
-        static SortedDictionary<BigInteger, Amogus> bb;
-        public static BigInteger ToBigInteger(string value)
-        {
-            BigInteger result = 0;
-            for (int i = 0; i < value.Length; i++)
-            {
-                result = result * 10 + (value[i] - '0');
-            }
-            return result;
-        }
         static void Main(string[] args)
         {
             int tests = int.Parse(Console.ReadLine());
@@ -36,15 +14,28 @@ namespace SBANK
             while (counter < tests)
             {
                 howManyAccounts = int.Parse(Console.ReadLine());
-                aa = new Dictionary<BigInteger, Amogus>();
+                x = new string[howManyAccounts];
                 for (var i = 0; i < howManyAccounts; i++)
                 {
                     s = Console.ReadLine();
-                    BigInteger ss = ToBigInteger(s);
-                    if (!aa.TryAdd(ss, new Amogus(s))) aa[ss].ammount++;
+                    x[i] = s;
                 }
-                bb = new SortedDictionary<BigInteger, Amogus>(aa);
-                foreach (var account in bb) Console.WriteLine(account.Value.key + account.Value.ammount);
+                Array.Sort(x, (x, y) => string.CompareOrdinal(x, y));
+                int iterum = 1;
+                for (var i = 1; i < howManyAccounts; i++)
+                {
+                    if (string.CompareOrdinal(x[i], x[i - 1]) == 0)
+                    {
+                        iterum++;
+                        if (i + 1 == howManyAccounts) Console.WriteLine(x[i] + iterum);
+                    }
+                    else
+                    {
+                        Console.WriteLine(x[i - 1] + iterum);
+                        iterum = 1;
+                        if (i + 1 == howManyAccounts) Console.WriteLine(x[i] + iterum);
+                    }
+                }
                 if (counter + 1 != tests)
                 {
                     Console.ReadLine();
