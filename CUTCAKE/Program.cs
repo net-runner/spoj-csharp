8632b7ed4011c4057dcd580a0efc8eaf3d6161dd
net-runner <slazynski.j@gmail.com>
2021-12-03 00:10:01 +0100

    Done

---
+++ CUTCAKE/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CUTCAKE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal n = decimal.Parse(Console.ReadLine());
+            for (decimal i = 0; i < n; i++) Console.WriteLine(Math.Ceiling((-1 + (Math.Sqrt(1 - (4 * (2 - double.Parse(Console.ReadLine()) * 2))))) / 2));
+        }
+    }
+}
